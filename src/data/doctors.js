ca35fcbbfb41affb1fd4020ea6ee675e5d4a6342
Chakri-commits <gchakri2003@gmail.com>
2025-08-02 14:06:23 +0530

    Create doctors.js

---
+++ src/data/doctors.js
@@ -0,0 +1,44 @@
+const doctors = [
+  {
+    id: 1,
+    name: "Dr. Satyanarana",
+    specialization: "Cardiologist",
+    image: "https://wallpapercave.com/wp/wp2968524.jpg",
+    available: true
+  },
+  {
+    id: 2,
+    name: "Dr. Arjun Mehta",
+    specialization: "Dermatologist",
+    image: "https://wallpapers.com/images/hd/doctor-pictures-l5y1qs2998u7rf0x.jpg",
+    available: false
+  },
+  {
+    id: 4,
+    name: "Dr. Sneha Rao",
+    specialization: "Pediatrician",
+    image: "https://static.vecteezy.com/system/resources/previews/026/375/249/non_2x/ai-generative-portrait-of-confident-male-doctor-in-white-coat-and-stethoscope-standing-with-arms-crossed-and-looking-at-camera-photo.jpg",
+    available: true
+  },
+  {
+    id: 5,
+    name: "Dr. Rahul Singh",
+    specialization: "Orthopedic",
+    image: "https://as2.ftcdn.net/v2/jpg/02/60/04/09/1000_F_260040900_oO6YW1sHTnKxby4GcjCvtypUCWjnQRg5.jpg",
+    available: true
+  },
+  {
+    id:6,
+    name:"Dr. Srihari",
+    specialization: "parademic",
+    available:false
+  },
+  {
+    id:6,
+    name:"Dr. Jayaraj",
+    specialization: "pyschologist",
+    available:true
+  }
+];
+
+export default doctors;
